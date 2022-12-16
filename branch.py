1760d3cdff313d3aa0af404c1a93997ccda02cbe
Letícia Nóbrega <120648652+lleticianobrega@users.noreply.github.com>
2022-12-16 09:06:09 -0300

    commit de conteudo
    estou fazendo um comiit da branch conteudo

--- branch.py
+++ branch.py
@@ -1,3 +1,13 @@
-print('a')
-print('b')
-print('c')
+print('estou modificando a minha branch conteudo')
+class conteudo:
+    def comandoLista(self):
+
+        lista = []
+        for recebe in range(2):
+            nome = input('nome: ')
+            idade = int(input('idade: '))
+            profissao = input('prof: ')
+            lista.append(nome)
+            lista.append(idade)
+            lista.append(profissao)
+        print(lista)
